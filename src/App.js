5fe826bb5c6d76e9e02e259a27fc68de23761a97
Marisa Wong <marisawong@berkeley.edu>
2019-07-21 16:25:27 -0400

    Initial shit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos'; 
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hay</h1>
+        <Todos />
+      </div>
+    );
+  }
+}
+
+export default App;
